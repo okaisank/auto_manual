73fd9e53827207346a91b5d75828d516338d4abf
Joevohan261 <84632709+okaisank@users.noreply.github.com>
2023-10-11 20:26:58 +0700

    Update Auto-manual_2.ino

--- Auto-manual_2.ino
+++ Auto-manual_2.ino
@@ -1,15 +1,17 @@
 #include <DHT.h>
 
-#define DHTPIN 2 // Digital pin where the DHT sensor is connected
+#define DHTPIN 15 // Digital pin where the DHT sensor is connected
 #define DHTTYPE DHT22 // Type of the DHT sensor
 
 DHT dht(DHTPIN, DHTTYPE);
-int relayPin = 4; // Digital pin connected to the relay
-int buttonPin = 5; // Digital pin connected to the push-button switch
+int relayPin = 32; // Digital pin connected to the relay
+int buttonPin = 16; // Digital pin connected to the push-button switch
 int mode = 1; // 1 for auto mode, 0 for manual mode
 bool manualStop = false;
 
 void setup() {
+
+  Serial.begin(115200);
   dht.begin();
   pinMode(relayPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor for the button
@@ -18,9 +20,19 @@ void setup() {
 void loop() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
-
+    if (isnan(temperature) || isnan(humidity)){ 
+  Serial.println("Read from DHT Sensor Failed ");
+  return;
+  }
+  Serial.print("\n"); 
+  Serial.print("Temperature : "); 
+  Serial.println(temperature); 
+  
+  Serial.print("Humidity : "); 
+  Serial.println(humidity); 
+  delay(1000);
   // Check if the button is pressed to toggle between auto and manual modes
-  if (digitalRead(buttonPin) == LOW) {
+  if (digitalRead(buttonPin) == HIGH) {
     mode = 1 - mode; // Toggle between 0 and 1
     delay(500); // Debounce delay
   }
@@ -29,17 +41,26 @@ void loop() {
   if (mode == 0 && digitalRead(buttonPin) == LOW) {
     manualStop = true;
     digitalWrite(relayPin, LOW); // Turn off the pump
+    
   }
 
   if (!manualStop) {
     if (mode == 1) { // Auto mode
       if (temperature > 27 && humidity > 80) {
-        digitalWrite(relayPin, LOW); // Turn off the pump
-      } else {
+    //if (temperature > 35 && humidity > 90) {  
         digitalWrite(relayPin, HIGH); // Turn on the pump
+        Serial.print(" Run Auto ");
+      } else {
+        digitalWrite(relayPin, LOW); // Turn off the pump
+        Serial.print(" Mode Auto ");
       }
     } else { // Manual mode
       // Add manual control logic here if needed
-    }
+      
+      digitalWrite(relayPin, LOW); // Turn on the pump
+      Serial.print(" Mode Manual");
+   
+
   }
+ }
 }
